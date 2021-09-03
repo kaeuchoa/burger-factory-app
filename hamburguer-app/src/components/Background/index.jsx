9428d4338144e334dae445621efc68113aab358a
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2021-09-03 01:45:14 -0300

    background

---
+++ hamburguer-app/src/components/Background/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HamburguerImg from '../img/hamburguerImg.svg'
+import './index.css'
+
+export default ()=>
+    <div className="container">
+        <div className="hamburguerArea">
+            <h2>Big Burguer</h2>
+            <img src={HamburguerImg} alt="" />
+            
+        </div>
+        <div>
+           
+        </div>
+    </div>
