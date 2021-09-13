6f50436d2073416c97ee9ae61056d6fdafd04f6b
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2021-09-13 17:23:45 -0300

    retornando os valore s do products

--- hamburguer-app/src/components/ChooseBox/ChooseSection/index.jsx
+++ hamburguer-app/src/components/ChooseBox/ChooseSection/index.jsx
@@ -3,15 +3,14 @@ import './index.css'
 import ProductsSection from '../ProductsSection'
 const ChooseSection = (props)=>{
     const options = props.options
-
     return(
         <>
             <div className='optionsName'>
                 {options.name}
             </div>      
-            <div className="productsSection">
-                <ProductsSection key={options.id}/>
-            </div>
+            {/* <div className="productsSection">  */}
+                 {/* <ProductsSection key={options.id}/> */}
+            {/* </div>  */}
         </>
     )
 }
--- hamburguer-app/src/components/ChooseBox/index.jsx
+++ hamburguer-app/src/components/ChooseBox/index.jsx
@@ -6,17 +6,19 @@ import ProductsSection from './ProductsSection'
 const ChooseBox = (props) => {
     const optionsList = props.optionsList
     const productsList = props.productsList
-    // console.log(productsList[1])
-    // console.log(optionsList)
+    
     return (
         <div className='containerBox'>
             <div className="chooseContainer">
                 {optionsList.map((options) => {
                     return <ChooseSection options={options} key={options.id} />
                 })} 
+            </div>
+            
+            <div className="containerProducts">
                 {productsList[1].map((products) => {
-                    <ProductsSection products={products} key={products.id} />
-                })} 
+                    return  <ProductsSection products={products} key={products.id} />
+                })}
             </div>
             <button className='sendButton'>Finalizado</button>
         </div>
