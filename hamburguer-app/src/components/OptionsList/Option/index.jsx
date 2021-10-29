859c3856c96a1845949c965e91c89d63a46eb2bc
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2021-10-28 23:51:45 -0300

    Add ao apertar nas opções aparece os produtos

--- hamburguer-app/src/components/OptionsList/Option/index.jsx
+++ hamburguer-app/src/components/OptionsList/Option/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import './index.css'
 import ProductsSection from '../ProductsSection'
+import {useState} from 'react'
 
 const Option = (props)=>{
     // para abrir e fechar(mudar a classe css), usar o state
+    const [show, setShow] = useState(true)
+    
+    function toggle() {
+        setShow(show => !show);
+    }
+      console.log(show)
     return(
         <>
             <div className='optionsName'>
-                {props.optionsName}
-            </div>      
-            <div className="productsSection"> 
-                 <ProductsSection products={props.products}/>
-                 {console.log('entrou')}
+                <button onClick={toggle}>{props.optionsName}</button>
+            </div>  
+               
+            <div className="productsSection">
+                {
+                    show?<ProductsSection products={props.products}/> :null
+                } 
+                 
             </div> 
         </>
     )
--- hamburguer-app/src/components/OptionsList/ProductsSection/index.jsx
+++ hamburguer-app/src/components/OptionsList/ProductsSection/index.jsx
@@ -4,11 +4,11 @@ import { useState } from 'react'
 
 const ProductsSection = (props)=>{
     const [count, setCount] = useState(0);
-    
+
     return(
         props.products.map((products)=>{
             return(
-                <div className='containerProducts'>
+                <div className='containerProducts' >
                     <button onClick={()=>setCount(count - 1)}>-1</button>
                     <input type="text" value={count}/>
                     <button onClick={()=>setCount(count + 1)}>+1</button>
