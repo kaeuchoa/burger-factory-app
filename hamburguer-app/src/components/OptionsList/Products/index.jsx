24012d48b4e5976a54bf04ebbb481ee6f4f823d6
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2021-11-08 18:03:00 -0300

    add limitador de produtos

--- hamburguer-app/src/components/OptionsList/Products/index.jsx
+++ hamburguer-app/src/components/OptionsList/Products/index.jsx
@@ -3,12 +3,22 @@ import './index.css'
 
 const Products = (props) => {
     const [count, setCount] = useState(0);
-
+    
+    function decrementCount(){
+        if(count > 0){
+            setCount(count - 1)
+        }  
+    }
+    function incrementCount(){
+        if(count < 4){
+            setCount(count + 1)
+        }  
+    }
     return (
         <div className='containerProducts'>
-            <button onClick={() => setCount(count - 1)}>-1</button>
-            <input type="text" value={count} />
-            <button onClick={() => setCount(count + 1)}>+1</button>
+            <button onClick={decrementCount}>-1</button>
+            <input type="text" value={count}/>
+            <button onClick={incrementCount}>+1</button>
             <p>{props.name}</p>
         </div>
     )
