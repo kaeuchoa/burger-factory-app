9eecc995c6b28a0b3a4072f7393a8b07f69df57c
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2021-11-08 17:21:14 -0300

    ajeitado contagem dos produtos

--- hamburguer-app/src/components/OptionsList/ProductsSection/index.jsx
+++ hamburguer-app/src/components/OptionsList/ProductsSection/index.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import './products.css'
-import { useState } from 'react'
+import Products from './Products'
+
 
 const ProductsSection = (props)=>{
-    const [count, setCount] = useState(0);
-
+  
     return(
-        props.products.map((products)=>{
-            return(
-                <div className='containerProducts'>
-                    <button onClick={()=>setCount(count - 1)}>-1</button>
-                    <input type="text" value={count}/>
-                    <button onClick={()=>setCount(count + 1)}>+1</button>
-                    <p>{products.name}</p>
-                </div>
-            )
-        })
+        props.products.map((product)=> <Products name={product.name}/>)
     )   
 }
 
